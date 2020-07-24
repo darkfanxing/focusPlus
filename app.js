dd8b14b462fa0c19f7f37ff4bbf09732a48ed49e
EasonC13 <gtes9950406@gmail.com>
2020-07-24 15:58:13 +0800

    added

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("QQ")
+
+
+
+// 讀取API
+
+url = "http://localhost:13523/getImg"
+data = fetch(url)
+            .then(function(response) {
+                data = response.json().then(function(data){
+                    $.getJSON(data.filename, function(json) { // 讀取本地 Json 檔案，裡面可以有任意物件
+                        console.log(json);
+                    });
+                    
+                })
+            })
